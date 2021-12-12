c8d95e2a4cb9f3f74502a3e27fce9aaa5f63a2a3
michellebinder <binder_michelle@web.de>
2021-12-12 13:28:45 +0100

    added homework 9

---
+++ src/hw9/main/FizzBuzz.java
@@ -0,0 +1,18 @@
+package main;
+
+public class FizzBuzz {
+	
+	public String convertNumber(int number) {
+		
+		if(number % 5 == 0 && number % 7 != 0) {
+			return "Fizz";
+		} else if(number % 7 == 0 && number % 5 != 0) {
+			return "Buzz";
+		} else if(number % 5 == 0 && number % 7 == 0) {
+			return"FizzBuzz";
+		} else {
+			return "";
+		}
+		
+	}
+}
